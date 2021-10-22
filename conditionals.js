fe2f9d899a270f6f40691eb3e46ba1f2afd7bab6
unknown <jaboguma@gmail.com>
2021-10-22 09:56:47 -0600

    Use conditionals to tell about weather

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+// const raining = false;
+// const cold = false;
+const temperature = 18;
+
+if (temperature < 0){
+  console.log(" Pick out your scarf!");
+}
+
+if (temperature < 15){
+  console.log("Short sleeve won't cat it!")
+}else {
+  console.log("Short sleeve is fine!");
+}
