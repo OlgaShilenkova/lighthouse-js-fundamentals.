9e8532ec57ff76c03ece993064d208b4787ec480
unknown <jaboguma@gmail.com>
2021-10-22 19:02:31 -0600

    If -else -if statement for diff aducation options and salary

---
+++ whichSchool.js
@@ -0,0 +1,38 @@
+/*const whichSchool  = function (age) {
+  let school = "";
+  if (age < 13) {
+    school = "Elementary school";
+    // console.log("Elementary School");
+    
+  } else if (age >= 13 && age <= 18) {
+    school = "Secondary school";
+    // console.log("Secondary School");
+    
+  } else if (age > 18 && age < 100) {
+    school = "Lighthouse Labs"; 
+    // console.log("Ligthouse Labs");
+  }
+  return school;
+};
+*/
+/*
+const whichSchool  = function (age) {
+   if (age < 13) {
+    //  School from capital S
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else if (age > 18 && age < 100) {
+  return "Lighthouse Labs";
+  }
+};
+
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
+
+*/
